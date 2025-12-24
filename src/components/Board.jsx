1bfb723f2286954115dd1348374ed494382b1bd7
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-24 17:25:00 +0530

    feat: update Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,18 @@
 import Square from "./Square";
+import calculateWinner from "../utils/calculateWinner";
 
 export default function Board({ board, onSquareClick }) {
-    return (
-        <div className="board">
-            {board.map((value, idx) => (
-                <Square
-                    key={idx}
-                    value={value}
-                    onClick={() => onSquareClick(idx)}
-                />
-            ))}
-        </div>
+    const result = calculateWinner(board);
+    const winningLine = result?.winningLine || [];
+
+    const renderSquare = (idx) => (
+        <Square
+            key={idx}
+            value={board[idx]}
+            onClick={() => onSquareClick(idx)}
+            highlight={winningLine.includes(idx)}
+        />
     );
+
+    return <div className="board">{board.map((_, idx) => renderSquare(idx))}</div>;
 }
